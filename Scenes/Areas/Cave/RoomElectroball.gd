e7f779b87eec459da22b6af28dc678722fdb0f17
browncanstudios <browncanstudios@gmail.com>
2021-09-17 17:07:18 -0700

    fixed minor bugs

--- Scenes/Areas/Cave/RoomElectroball.gd
+++ Scenes/Areas/Cave/RoomElectroball.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# TODO: think about what this looks like with more iron blocks!
 	var charge_diff = abs($IronBlock.charge - $IronBlockBig.charge)
 	var distance = $IronBlock.position.distance_to($IronBlockBig.position)
 	var force_magnitude = charge_diff / (distance * distance)
 	var force = force_magnitude * ($IronBlockBig.position - $IronBlock.position).normalized()
-	$IronBlock.add_central_force(force)
+	$IronBlock.apply_central_impulse(force)
